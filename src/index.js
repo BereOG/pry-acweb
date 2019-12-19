3b31275f3ea9646b5fc3aaae98e0750af39a9836
anverK <karinaagp26@gmail.com>
2019-12-19 00:17:07 -0600

    Se corrigen rutas para administrador

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 // Routes
 app.use(require('./routes'));
 app.use(require('./routes/authentication'));
-app.use('/links', require('./routes/links'));
+app.use('/admin', require('./routes/admin'));
 
 
 // Public
